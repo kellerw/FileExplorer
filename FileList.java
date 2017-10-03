8e6a36069323e10ebc78dba82619e806ab843a46
Thor Keller <thorboomafoo@gmail.com>
2017-10-02 21:45:13 -0400

    fixed code so it compiles...

--- FileList.java
+++ FileList.java
@@ -1,9 +1,13 @@
+import javafx.scene.control.ScrollPane;
+import javafx.scene.paint.Color;
+import javafx.scene.layout.Pane;
+
 public class FileList extends ScrollPane
 {
 	private Theme theme;
   private String directory;
 	private Pane content = new Pane();
-  public FileList(String directory, theme theme)
+  public FileList(String directory, Theme theme)
   {
     this.theme = theme;
     this.directory = directory;
